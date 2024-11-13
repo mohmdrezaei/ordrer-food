66aee9f9ce041d916f160900813c89d6a2ea7d8c
mohmdrezaei <mhwmdrz9@gmail.com>
2024-11-14 02:08:27 +0330

    Create detail page and display data

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -1,46 +1,45 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import { useRouter } from "next/router";
+import React from "react";
+import DetailsPage from "../../components/templates/DetailsPage";
 
-function Details({food}) {
-    console.log(food)
-    const router= useRouter()
-    if(router.isFallback) {
-        return <h2>Loading Page...</h2>
-    }
-  return (
-    <div>Details</div>
-  )
+function Details({ food }) {
+  console.log(food);
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h2>Loading Page...</h2>;
+  }
+  return <DetailsPage food={food} />;
 }
 
-export default Details
+export default Details;
 
 export async function getStaticPaths() {
-    const res = await fetch('http://localhost:4000/data')
-    const json = await res.json()
-    const data  = json.slice(0,10)
-    const paths = data.map((food) => ({
-        params: { id: food.id.toString() },
-    }))
+  const res = await fetch("http://localhost:4000/data");
+  const json = await res.json();
+  const data = json.slice(0, 10);
+  const paths = data.map((food) => ({
+    params: { id: food.id.toString() },
+  }));
 
-    return {
-        paths,
-        fallback: true,
-    }
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
 export async function getStaticProps(context) {
-   const {id}= context.params;
-   const res = await fetch(`http://localhost:4000/data/${id}`)
-   const data = await res.json()
+  const { id } = context.params;
+  const res = await fetch(`http://localhost:4000/data/${id}`);
+  const data = await res.json();
 
-   if(!data.id){
-       return{
-           notFound: true
-       }
-   }
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
 
-   return {
-       props: { food: data },
-       revalidate:10
-   }
+  return {
+    props: { food: data },
+    revalidate: 10,
+  };
 }
